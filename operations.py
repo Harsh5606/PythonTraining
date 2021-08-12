388ab92ec24e0d5aa1156f505bb4e3cb07c4decf
Harsh5606 <88761766+Harsh5606@users.noreply.github.com>
2021-08-12 12:06:11 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+
+# Addition
+print("Adition is ",a+b)
+
+# Subtraction
+print("Subtraction  is ",a-b)
+
+# Multiplication
+print("Multiplication  is ",a*b)
+
+# Division
+print("Division is ",a/b)
+
+# Mod
+print("Mod is ",a%b)
+
+# power
+print("power is ",a**b)
