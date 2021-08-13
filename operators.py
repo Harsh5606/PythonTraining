abf0c5c68e6ffe0521ead8daee120dc6bc67146a
Harsh Upadhye <88761766+Harsh5606@users.noreply.github.com>
2021-08-13 12:57:51 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,70 @@
+# Arithmetic operators
+x = 15
+y = 4
+
+print("Arithmetic operators")
+
+
+print('x + y =',x+y)
+
+print('x - y =',x-y)
+
+print('x * y =',x*y)
+
+print('x / y =',x/y)
+
+print('x // y =',x//y)
+
+print('x ** y =',x**y)
+
+
+#Comparison operators
+print("Comparison operators")
+
+print('x > y is',x>y)
+
+print('x < y is',x<y)
+
+print('x == y is',x==y)
+
+print('x != y is',x!=y)
+
+print('x >= y is',x>=y)
+
+print('x <= y is',x<=y)
+
+
+# Logical operators
+
+print("Logical operators")
+
+print('x and y is',x and y)
+
+print('x or y is',x or y)
+
+print('not x is',not x)
+
+
+#Identity operators
+
+print("Identity operators")
+x1=3
+x2="hey"
+x3=[3,4,5,6]
+y1=3
+y2="hey"
+y3=[3,4,5,6]
+print("\n x1 is not y2",x1 is not y2)
+print("\n x1 is not y1",x1 is not y1)
+print("\n x2 is y2",x1 is y2)
+print("\n x3 is  y2",x2 is  y2)
+ 
+#Membership operators
+print("Membership operators")
+
+a1="Hey"
+a2={1:'a',2:'b'}
+print("\n" 'H' in a1)
+print("\n "'hello' not  in a1)
+print(1 in a2)
+print('a' in a2)
